5d91391c7b93a29c6f3a72b34c834fd063e60875
peunj04 <peunj04@gmail.com>
2020-06-10 22:21:40 +0900

    새 액티비티 추가

---
+++ app/src/main/java/com/example/signuppractice_20200408/MyPageActivity.kt
@@ -0,0 +1,13 @@
+package com.example.signuppractice_20200408
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MyPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page)
+    }
+
+}
