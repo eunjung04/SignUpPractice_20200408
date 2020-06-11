ce23894e02f0e33376684fb26ec541d09f0fae01
peunj04 <peunj04@gmail.com>
2020-06-11 21:11:55 +0900

    다음 버튼 누르면=>마이페이지로 이동

--- app/src/main/java/com/example/signuppractice_20200408/MainActivity.kt
+++ app/src/main/java/com/example/signuppractice_20200408/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.signuppractice_20200408
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nextBtn.setOnClickListener {
+
+            val myIntent= Intent(this, MyPageActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
