88e9bba2e394502988ec1c3abcd60a560a2ddefe
peunj04 <peunj04@gmail.com>
2020-06-19 20:32:46 +0900

    데이터 첨부 예시

--- app/src/main/java/com/example/signuppractice_20200408/MyPageActivity.kt
+++ app/src/main/java/com/example/signuppractice_20200408/MyPageActivity.kt
@@ -2,12 +2,17 @@ package com.example.signuppractice_20200408
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_my_page.*
 
 class MyPageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
+
+        val name=intent.getStringExtra("userName")
+
+        userNameTxt.text=name
     }
 
 }
