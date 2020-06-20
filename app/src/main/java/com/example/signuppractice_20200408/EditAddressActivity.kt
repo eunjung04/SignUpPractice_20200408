2a6d474ec746e931fb659544129146e5cf0af694
peunj04 <peunj04@gmail.com>
2020-06-20 15:27:09 +0900

    주소 입력 화면 그리기

---
+++ app/src/main/java/com/example/signuppractice_20200408/EditAddressActivity.kt
@@ -0,0 +1,13 @@
+package com.example.signuppractice_20200408
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EditAddressActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_address)
+    }
+}
